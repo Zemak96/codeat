20caa12841a122112a7ff5c89e16009fab5d3d6d
LorenaMMart <lorena.martinezbis1@gmail.com>
2024-02-10 21:11:56 +0100

    estructura cursos y comunidad

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CursoComponent } from '../curso/curso.component';
+import { BdCursosService } from '../bd-cursos.service';
+
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [CursoComponent],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
 export class CursosComponent {
+  private curBD = inject(BdCursosService)
+  getCursos(){
+    return this.curBD.getCursos()
+  }
 
 }
