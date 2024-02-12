9237fbecec968c50847d8609e427e4718fb0993b
Antonio <antoniosola991@gmail.com>
2024-02-13 00:29:01 +0100

    Deshacer contacto de footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
@@ -9,13 +8,5 @@ import { Router } from '@angular/router';
     styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-    constructor(private router: Router) {
-    }
 
-    ngOnInit() {
-    }
-
-    irAContacto() {
-        this.router.navigate(['contacto'])
-    }
 }
