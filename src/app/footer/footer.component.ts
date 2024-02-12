6f311de71ce2f6a200c2b5d31bd563a6fa4cb118
Antonio <antoniosola991@gmail.com>
2024-02-13 00:27:27 +0100

    Arreglado contacto footer 2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 
 @Component({
     selector: 'app-footer',
@@ -11,6 +10,9 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent {
     constructor(private router: Router) {
+    }
+
+    ngOnInit() {
     }
 
     irAContacto() {
