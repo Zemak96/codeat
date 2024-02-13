e1867a7efe4d0acc7fba553aa90a2dc76ad9c079
Zaén <zaenmapercu@gmail.com>
2024-02-13 19:55:30 +0100

    Transcripciones

--- src/app/comunidad/comunidad.component.ts
+++ src/app/comunidad/comunidad.component.ts
@@ -9,7 +9,16 @@ import { Router } from '@angular/router';
   styleUrl: './comunidad.component.css'
 })
 export class ComunidadComponent {
+  private transcripcion = "(Música)Te damos la bienvenida a nuestros cursos online. Aprende y crece junto a nosotros. Nuevos métodos de aprendizaje. Nuestros mejores cursos: Javascript, Java, HTML5, PHP, Angular, Symfony. Invierte para ti y en tu futuro. Únete a nosotros. CodeaT.(Música)"
+  private showTranscripcion: boolean = false
   constructor(private router: Router){
+    
+  }
 
+  getTranscripcion(){
+    return this.transcripcion
+  }
+  mostrarTranscripcion(){
+    return this.showTranscripcion = !this.showTranscripcion
   }
 }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CursoComponent } from '../curso/curso.component';
 import { BdCursosService } from '../bd-cursos.service';
 import { Router } from '@angular/router';
@@ -11,14 +11,23 @@ import { Router } from '@angular/router';
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
+export class CursosComponent{
   
   private curBD = inject(BdCursosService)
+  private transcripcion = "(Música)Te damos la bienvenida a nuestros cursos online. Aprende y crece junto a nosotros. Nuevos métodos de aprendizaje. Nuestros mejores cursos: Javascript, Java, HTML5, PHP, Angular, Symfony. Invierte para ti y en tu futuro. Únete a nosotros. CodeaT.(Música)"
+  private showTranscripcion: boolean = false
   constructor(private router: Router){
     
   }
+
   getCursos(){
     return this.curBD.getCursos()
+  }
+  getTranscripcion(){
+    return this.transcripcion
+  }
+  mostrarTranscripcion(){
+    return this.showTranscripcion = !this.showTranscripcion
   }
 
 }
